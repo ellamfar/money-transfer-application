74ce51f361441a0190e8692b1be565779b9daac1
Ella <ella@smartcow.ai>
2022-11-16 06:24:05 +0100

    pointed the java container to the mysql container - Changes the url property to include mysql container instead of local host. - Test was failing during build so changed the springboot test properties in   the test class. These properties override the main properties in application.properties.

--- src/main/java/com/ellamfar/moneytransferapplication/InsufficientFundsAdvice.java
+++ src/main/java/com/ellamfar/moneytransferapplication/InsufficientFundsAdvice.java
@@ -1,10 +1,8 @@
 package com.ellamfar.moneytransferapplication;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 class InsufficientFundsAdvice {
--- src/test/java/com/ellamfar/moneytransferapplication/MoneyTransferApplicationTests.java
+++ src/test/java/com/ellamfar/moneytransferapplication/MoneyTransferApplicationTests.java
@@ -3,7 +3,9 @@ package com.ellamfar.moneytransferapplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = { 
+	"spring.datasource.url=jdbc:mysql://localhost:3306/app_accounts_db?createDatabaseIfNotExist=true&useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false",
+	"spring.datasource.username=moneytransfer_user" })
 class MoneyTransferApplicationTests {
 
 	@Test
